dc4cff90ae9086b7ab93501fd8e41275dab18153
Chinaemerem Agomoh <chinaemerem98@yahoo.com>
2023-01-04 16:22:08 +0100

    finished login screen

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,49 @@
 import Input from "../components/Input"
-import InputWithSearch from "../components/InputWithSearch"
+import { useNavigate, Link } from "react-router-dom"
+import "../styles/login.scss"
+
+/// <reference types="vite-plugin-svgr/client" />
+import { ReactComponent as Logo } from "../assets/icons/logo.svg"
+import banner from "../assets/banner.svg"
 
 const Login = () => {
+	const navigate = useNavigate()
+
 	return (
-		<>
-			<Input
-				placeholder='enter'
-				type='select'
-				label='username'
-				// isAuth
-			/>
-			<InputWithSearch placeholder='enter' />
-		</>
+		<div className='login'>
+			<div className='login__container'>
+				<div className='login__images'>
+					<div className='login__logo'>
+						<Logo />
+					</div>
+					<img
+						src={banner}
+						alt='Banner'
+					/>
+				</div>
+				<div className='login__form'>
+					<div className='login__form-header'>
+						<h1>Welcome!</h1>
+						<h2>Enter details to login.</h2>
+					</div>
+					<div className='login__input-fields'>
+						<Input
+							type='text'
+							isAuth
+							placeholder='Email'
+						/>
+						<Input
+							type='text'
+							isAuth
+							placeholder='Email'
+						/>
+
+						<Link to={"#"}>forgot password</Link>
+					</div>
+					<button className='login__button'>log in</button>
+				</div>
+			</div>
+		</div>
 	)
 }
 
